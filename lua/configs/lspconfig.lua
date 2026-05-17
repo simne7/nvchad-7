9b69636fb4eca34ed028c01707f5a1391f2b0dfd
simne7 <simne7@gmail.com>
2026-05-17 13:50:16 +0200

    feat: add denols lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -18,23 +18,55 @@ nvlsp.on_attach = function(_, bufnr)
   map("n", "<leader>rr", require "nvchad.lsp.renamer", { buffer = bufnr, desc = "Rename" })
 end
 
+---Specialized root pattern that allows for an exclusion
+---@param opt { root: string[], exclude: string[] }
+---@return fun(bufnr: integer, on_dir: fun(dir: string|nil))
+---@see https://www.npbee.me/posts/deno-and-typescript-in-a-monorepo-neovim-lsp
+local function root_pattern_exclude(opt)
+  local lsputil = require('lspconfig.util')
+
+  return function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local excluded_root = lsputil.root_pattern(opt.exclude)(fname)
+    if not excluded_root then
+      on_dir(lsputil.root_pattern(opt.root)(fname))
+    end
+  end
+end
+
 -- see https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#configuration
 -- customization ideas: https://rishabhrd.github.io/jekyll/update/2020/09/19/nvim_lsp_config.html
 local servers = {
-  jsonls = {},
-  tailwindcss = {},
-  ts_ls = {},
-  eslint = {},
+  docker_compose_language_service = {},
   cssls = {},
+  denols = {
+    root_dir = root_pattern_exclude({
+      root = { "deno.json", "deno.jsonc", "deno.lock" },
+      exclude = {}
+    }),
+    single_file_support = false,
+    settings = {},
+  },
+  eslint = {},
   html = {},
+  jsonls = {},
   pylsp = {},
+  -- tailwindcss = {},
+  ts_ls = {
+    root_dir = root_pattern_exclude({
+      root = { "package.json" },
+      exclude = { "deno.json", "deno.jsonc", "deno.lock" }
+    }),
+    single_file_support = false,
+    settings = {},
+  },
 }
 
 for name, opts in pairs(servers) do
+  vim.lsp.enable(name)
   if next(opts) ~= nil then
     vim.lsp.config(name, opts)
   end
-  vim.lsp.enable(name)
 end
 
 local M = {}
