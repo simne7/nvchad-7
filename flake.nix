8c92c7024229c41aa377d95037127085814c275a
simne7 <simne7@gmail.com>
2026-06-12 17:28:41 +0200

    chore: add claude tooling

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 {
   description = "NixOS docker image";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
@@ -15,13 +15,20 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        #pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { 
+          inherit system;
+          config.allowUnfree = true;
+        };
+
       in
       rec {
         # Development environment: nix develop
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             cargo
+            claude-code
+            claude-agent-acp
           ];
         };
       }
